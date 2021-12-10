34175cce980c3a8b67b70efa5b6e7f6e51fd06a2
Nick <a.nick.day@brightminded.com>
2021-12-10 13:51:09 +0000

    Further testing stages

--- tests/test_vocabularise.py
+++ tests/test_vocabularise.py
@@ -2,104 +2,135 @@ import unittest
 
 from vocabularise import Vocabularise
 
+from nltk.tokenize import word_tokenize
+
 class TestVocabularise( unittest.TestCase ):
 
-	def setUp( self ):
+    def setUp( self ):
 
-		self.V = Vocabularise()
-
-
-	def testTokenise( self ):
-
-		# when the input is a string of text a default tokenisation is used
-		text = "Good muffins cost $3.88\nin New-York.  Please buy me two of them.\n\nThanks."
-
-		expectedDefault = [ 'Good', 'muffins', 'cost', '$', '3.88', 'in', 'New-York', '.', 'Please', 'buy', 'me', 'two', 'of', 'them', '.', 'Thanks', '.' ]
-
-		actual = self.V.tokenise( text )
-
-		self.assertListEqual( actual, expectedDefault )
-
-		# when the input is a string text and a regex the resulting tokenisation 
-		# will honour the regular expression
-		regex = r'[\d\w]+'
-		
-		expectedWRegex = [ 'Good', 'muffins', 'cost', '3', '88', 'in', 'New', 'York', 'Please', 'buy', 'me', 'two', 'of', 'them', 'Thanks' ]
-
-		actual = self.V.tokenise( text, regex )
-
-		self.assertListEqual( actual, expectedWRegex )
+        self.V = Vocabularise()
 
 
-	def testTokenCleanup( self ):
+    def testTokenise( self ):
 
-		dirtyToken = 'football22'
+        # when the input is a string of text a default tokenisation is used
+        text = "Good muffins cost $3.88\nin New-York.  Please buy me two of them.\n\nThanks."
 
-		expected = 'football'
+        expectedDefault = [ 'Good', 'muffins', 'cost', '$', '3.88', 'in', 'New-York', '.', 'Please', 'buy', 'me', 'two', 'of', 'them', '.', 'Thanks', '.' ]
 
-		regex = r'[A-Za-z]+'
+        actual = self.V.tokenise( text )
 
-		actual = self.V.tokenCleanup( dirtyToken, regex )
+        self.assertListEqual( actual, expectedDefault )
 
-		self.assertEqual( actual, expected )
+        # when the input is a string text and a regex the resulting tokenisation 
+        # will honour the regular expression
+        regex = r'[\d\w]+'
+        
+        expectedWRegex = [ 'Good', 'muffins', 'cost', '3', '88', 'in', 'New', 'York', 'Please', 'buy', 'me', 'two', 'of', 'them', 'Thanks' ]
 
+        actual = self.V.tokenise( text, regex )
 
-	def testTokensCleanup( self ):
-
-		dirty = [ 'football22', '33ironman', 'help,', '  venom ' ]
-
-		expected = [ 'football', 'ironman', 'help', 'venom' ]
-	
-		regex = r'[A-Za-z]+'
-
-		actual = self.V.tokensCleanup( dirty, regex )
-
-		self.assertListEqual( actual, expected )
+        self.assertListEqual( actual, expectedWRegex )
 
 
-	def testStem( self ):
+    def testTokenCleanup( self ):
 
-		vocab = [ 'football', 'footballs', 'reducted', 'reduction', 'shining', 'cat', 'cat', 'cats' ]
+        dirtyToken = 'football22'
 
-		expectedStems = [ 'footbal', 'reduct', 'shine', 'cat' ]
+        expected = 'football'
 
-		expectedMap = {
+        regex = r'[A-Za-z]+'
 
-			'footbal': [ 'football', 'footballs' ], 
-			'reduct': [ 'reducted', 'reduction' ], 
-			'shine': [ 'shining' ], 
-			'cat': [ 'cat', 'cats' ]
+        actual = self.V.tokenCleanup( dirtyToken, regex )
 
-		}
-		
-		actualStems, stem2word = self.V.stem( vocab )
-
-		self.assertListEqual( actualStems, expectedStems )
-
-		self.assertDictEqual( stem2word, expectedMap )
-
-	def testVocabularise( self ):
-
-		# in the default scenario the assumption is that
-		# stem=false, tokeniser=None, cleanup=None
-
-		corpus = [
-	
-			"Every time I thought I was being rejected from something good, I was actually being re-directed to something better.", 
-			"It is not so much the major events as the small day-to-day decisions that map the course of our living. . . Our lives are, in reality, the sum total of our seemingly unimportant decisions and of our capacity to live by those decisions.",
-			"Maybe 'Okay' will be our 'always'...", 
-			"When the world pushes you to your knees, you're in the perfect position to pray","Criticism , however valid or intellectually engaging , tends to get in the way of a writer who has anything personal to say. A tightrope walker may require practice, but if he starts a theory of equilibrium he will lose grace (and probably fall off)." 
-
-		]
-		
-		expectedVocabulary = ["Every", "time", "I", "thought", "was", "being", "rejected", "from", "something", "good", "actually", "re-directed", "to", "better", "it", "is", "not", "so", "much", "the", "major", "events", "as", "small", "day-to-day", "decisions", "that", "map", "course", "of", "our", "living", "lives", "are", "in", "reality", "sum", "total", "seemingly", "unimportant", "and", "capacity", "live", "by", "those", "Maybe", "'Okay'", "will", "be", "always", "When", "world", "pushes", "you", "your", "knees", "you're", "perfect", "position", "pray", "Criticism", "however", "valid", "or", "intellectually", "engaging", "tends", "get", "way", "a", "writer", "who", "has", "anything", "personal", "say", "A", "tightrope", "walker", "may", "require", "practice", "but", "he", "starts", "theory", "equilibrium", "lose", "grace", "probably", "fall", "off" ] 	
-		
-		actualVocabulary = self.V.vocabularise( corpus )
-
-		self.assertListEqual( actualVocabulary, expectedVocabulary )
+        self.assertEqual( actual, expected )
 
 
+    def testTokensCleanup( self ):
 
+        dirty = [ 'football22', '33ironman', 'help,', '  venom ' ]
+
+        expected = [ 'football', 'ironman', 'help', 'venom' ]
+    
+        regex = r'[A-Za-z]+'
+
+        actual = self.V.tokensCleanup( dirty, regex )
+
+        self.assertListEqual( actual, expected )
+
+
+    def testStem( self ):
+
+        vocab = [ 'football', 'footballs', 'reducted', 'reduction', 'shining', 'cat', 'cat', 'cats' ]
+
+        expectedStems = [ 'footbal', 'reduct', 'shine', 'cat' ]
+
+        expectedMap = {
+
+            'footbal': [ 'football', 'footballs' ], 
+            'reduct': [ 'reducted', 'reduction' ], 
+            'shine': [ 'shining' ], 
+            'cat': [ 'cat', 'cats' ]
+
+        }
+        
+        actualStems, stem2word = self.V.stem( vocab )
+
+        self.assertListEqual( actualStems, expectedStems )
+
+        self.assertDictEqual( stem2word, expectedMap )
+
+    def testVocabularise( self ):
+
+        # in the default scenario the assumption is that
+        # stem=False, tokeniser=None, cleanup=None.  The default
+        # tokeniser is nltk.word_tokenize.
+        
+        corpus = [
+     
+             "Every time I thought I was being rejected from something good, I was actually being re-directed to something better.", 
+             "It is not so much the major events as the small day-to-day decisions that map the course of our living. . . Our lives are, in reality, the sum total of our seemingly unimportant decisions and of our capacity to live by those decisions.",
+             "Maybe 'Okay' will be our 'always'...", 
+             "When the world pushes you to your knees, you're in the perfect position to pray",
+             "Criticism , however valid or intellectually engaging , tends to get in the way of a writer who has anything personal to say. A tightrope walker may require practice, but if he starts a theory of equilibrium he will lose grace (and probably fall off)." 
+
+        ]
+        
+        actualVocabulary = self.V.vocabularise( corpus )
+        
+        actualVocabularySet = set( actualVocabulary )
+        
+        for doc in corpus:
+            
+            expectedVocabulary = set( word_tokenize( doc.lower() ) )
+            
+            intersection = actualVocabularySet.intersection( expectedVocabulary )
+            
+            # Testing all created tokens are in the expected vocabulary.
+            # All return words should be in lower case.
+            
+            self.assertSetEqual(  intersection, expectedVocabulary )
+            
+        # Testing that a vocabulary contains no duplicates.
+        
+        self.assertEqual( len( actualVocabulary ), len ( actualVocabularySet ) )
+
+
+        # When a regular expression is supplied to
+        # the tokeniser, we expect the text to be 
+        # tokenise correctly.
+
+        expectedVocabulary = ["every", "time", "i", "thought", "was", "being", "rejected", "from", "something", "good", "actually", "re-directed", "to", "better", "it", "is", "not", "so", "much", "the", "major", "events", "as", "small", "day-to-day", "decisions", "that", "map", "course", "of", "our", "living", "lives", "are", "in", "reality", "sum", "total", "seemingly", "unimportant", "and", "capacity", "live", "by", "those", "maybe", "okay", "will", "be", "always", "when", "world", "pushes", "you", "your", "knees", "you're", "perfect", "position", "pray", "criticism", "however", "valid", "or", "intellectually", "engaging", "tends", "get", "way", "a", "writer", "who", "has", "anything", "personal", "say", "tightrope", "walker", "may", "require", "practice", "but", "he", "starts", "theory", "equilibrium", "lose", "grace", "probably", "fall", "off" ] 
+
+        regex = r'[\w]+((?!\s)\W?[\w]+)*'
+        
+        actualVocabulary = self.V.vocabularise( corpus, tokeniser=regex )
+
+        print( "Actual Vocab:", actualVocabulary )
+
+        self.assertListEqual( actualVocabulary, expectedVocabulary )
+        
+        
 
 
 
--- vocabularise.py
+++ vocabularise.py
@@ -6,58 +6,86 @@ from nltk.tokenize import word_tokenize, RegexpTokenizer
 
 class Vocabularise( object ):
 
-	def __init__( self ):
+    def __init__( self ):
 
-		self._stemmer = PorterStemmer()
+        self._stemmer = PorterStemmer()
 
-	def tokenise( self, text, regex=None ):
+    def tokenise( self, text, regex=None ):
 
-		if regex:
-			tokeniser = RegexpTokenizer( regex )
-
-			return tokeniser.tokenize( text )
-		
-		return word_tokenize( text )
-
-
-	def tokenCleanup( self, dirty, regex ):
-
-		result = re.findall( regex, dirty )
-
-		if result:
-
-			return result[ 0 ]
-
-		return ''
+        if regex:
+            tokeniser = RegexpTokenizer( regex )
+            
+            print("Text:", text )
+            
+            print("Result:", tokeniser.tokenize( text ) )
+            
+            return tokeniser.tokenize( text )
+        
+        return word_tokenize( text )
 
 
-	def tokensCleanup( self, dirty, regex ):
+    def tokenCleanup( self, dirty, regex ):
+        
+        if not regex:
+        
+            return dirty
+        
+        result = re.findall( regex, dirty )
 
-		result = [ self.tokenCleanup( token, regex ) for token in dirty ]
+        if result:
 
-		return [ token for token in result if token ]
+            return result[ 0 ]
 
-
-	def stem( self, vocab ):
-
-		stem2word = {}
-
-		for word in vocab:
-
-			stem = self._stemmer.stem( word )
-
-			if stem not in stem2word:
-
-				stem2word[ stem ] = []
-
-			if word not in stem2word[ stem ]:
-				
-				stem2word[ stem ].append( word )
-
-		return list( stem2word.keys() ), stem2word
+        return ''
 
 
+    def tokensCleanup( self, dirty, regex ):
 
+        result = [ self.tokenCleanup( token, regex ) for token in dirty ]
+
+        return [ token for token in result if token ]
+
+
+    def stem( self, vocab ):
+
+        stem2word = {}
+
+        for word in vocab:
+
+            stem = self._stemmer.stem( word )
+
+            if stem not in stem2word:
+
+                stem2word[ stem ] = []
+
+            if word not in stem2word[ stem ]:
+                
+                stem2word[ stem ].append( word )
+
+        return list( stem2word.keys() ), stem2word
+
+
+
+    def vocabularise( self, corpus, tokeniser=None, cleanup=None, stem=False ):
+        
+        vocab = []
+        
+        for doc in corpus:
+            
+            doc = doc.lower()
+            
+            tokenedVocab = self.tokenise( doc, regex=tokeniser )
+            
+            cleanedVocab = self.tokensCleanup( tokenedVocab, cleanup )
+            
+            vocab += cleanedVocab
+        
+        vocabList = list( set( vocab ) )
+
+        if stem:
+            vocabList, _  = self.stem( vocabList )
+
+        return vocabList
 
 
 
